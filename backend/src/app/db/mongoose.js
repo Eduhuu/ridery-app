59250a72ee25763a2b1645bdc8776f7c29fca487
Eduhuu <eduardosuarez.c97@gmail.com>
2024-10-22 13:25:21 -0400

    update docker compose and mongoose connection

--- backend/src/app/db/mongoose.js
+++ backend/src/app/db/mongoose.js
@@ -1,9 +1,12 @@
 import mongoose from 'mongoose';
+import dotenv from "dotenv"
+dotenv.config()
 
 export const connect_mongo = async () => {
     let connection = null
+    const mongo_uri = process.env.MONGO_URI || "mongodb://admin:admin@localhost:27017/mongo_db"
     try {
-        connection = await mongoose.connect('mongodb://admin:admin@localhost:27017/mongo_db',{
+        connection = await mongoose.connect(mongo_uri,{
             authSource: 'admin'
         })
         console.log('Conexión a MongoDB establecida');
